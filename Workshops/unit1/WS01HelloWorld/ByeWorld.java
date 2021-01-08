737eaee62f48293c14a8c5daa30ea591efd35b38
df-al12 <denisalquinga123@gmail.com>
2021-01-08 13:37:19 -0500

    WWS01HelloWorl in tjre e programmy lannguages

---
+++ Workshops/unit1/WS01HelloWorld/ByeWorld.java
@@ -0,0 +1,12 @@
+// Dennis Alquinga Hello World
+
+/*
+same lines of comments
+can be written this way 
+thanks
+*/
+class HelloWorld{
+     public static void main(String[] args){
+	     System.out.println("Hello World from Dennis Alquinga");
+	 }
+}
---
+++ Workshops/unit1/WS01HelloWorld/HelloWorld.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+     //printf() displays information
+     printf("Hello World \t \" from Dennis Alquinga! \" =) \n");
+	 printf("\ The grade is %d \n", grade);
+     returm 0;
+}	 
