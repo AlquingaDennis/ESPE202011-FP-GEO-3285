849ac15845d06d3a051d6960432aeee4eca235f5
df-al12 <denisalquinga123@gmail.com>
2021-01-15 14:50:45 -0500

    WS07Menus

---
+++ assignments/Unit1/HW03Iteration/JavaApplication20/src/javaapplication20/JavaApplication20.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication20;
+
+/**
+ *
+ * @author Asus
+ */
+public class JavaApplication20 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int i;
+        i=0;  //variable
+        final float pi = 3.14F;  //constante
+        System.out.println("pi -> " +pi);
+        float radio=1;
+        float area=pi * radio * radio;
+        
+    System.out.println("pi -> " +pi);
+    int number = 9;
+    float realNumber = 0.0F;
+    
+    // 7.7 -> float
+    // 7   -> int or float  
+        
+    realNumber = number; //casting conversion de datos de int a float
+        
+    System.out.println("integer number --> " + number);
+    System.out.println("float realNumber --> " + realNumber);
+    
+    realNumber =6.7F;
+    
+    number = (int) realNumber; //casting conversion de datos de float a int
+    
+    System.out.println("integer number --> " + number);
+    System.out.println("float realNumber --> " + realNumber);
+    
+    number = (int)
+    
+    
+            
+        
+        
+        
+        
+        
+    }
+    
+}
